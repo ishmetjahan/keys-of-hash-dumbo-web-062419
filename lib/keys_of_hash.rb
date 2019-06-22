3bdc3929d929996a37d0117470ae7d2035e9c670
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 22:23:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,12 @@
 class Hash
   def keys_of(arguments)
     arr= []
-    
+    Hash.each do |key, value|
+   arguments.each do |i|
+     if i == value 
+       arr.push (key)
   end
+end
+return arr
+end 
 end
